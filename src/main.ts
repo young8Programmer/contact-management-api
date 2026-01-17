d6eb3b6eeee26382bbb270ff4ec7f066061ea73e
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:16:58 +0500

    style: kod formatlash va indentatsiya

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 // kod formatlash va tozalash
+// kod formatlash va indentatsiya
 async function bootstrap() {
 // database testlari qo'shildi
   const app = await NestFactory.create(AppModule);
