68df20bfb5b5792a634903644919cd13d4328d66
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:16:10 +0500

    perf: database querylarni optimallashtirish

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
+// authentication xatosi tuzatildi
 import { ContactsService } from "./contacts.service";
 import { CreateContactDto } from "./dto/create-contact.dto";
 import { Contact } from "./entities/contact.entity";
