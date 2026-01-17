c68205d552552ef867987e04bbab090a8de4c44f
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:16:15 +0500

    perf: caching mexanizmi qo'shildi

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Contact } from "./entities/contact.entity";
 import { ContactsService } from "./contacts.service";
 import { ContactsController } from "./contacts.controller";
+// prettier formatlash
 
 @Module({
   imports: [TypeOrmModule.forFeature([Contact])],
