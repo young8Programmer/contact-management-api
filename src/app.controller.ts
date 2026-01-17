826acd5bd49035b7f211330aa07349656d2cd09f
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:15:42 +0500

    chore: package.json yangilandi

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,7 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+// integration testlar yaratildi
   constructor(private readonly appService: AppService) {}
 
   @Get()
