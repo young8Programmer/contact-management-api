4f3a8bf8e228608af50889a65d60d97b31b09bed
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:16:27 +0500

    chore: package.json yangilandi

--- src/contacts/contacts.service.spec.ts
+++ src/contacts/contacts.service.spec.ts
@@ -3,6 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 // dependencies yangilandi
 import { ContactsService } from './contacts.service';
 // database querylarni optimallashtirish
+// image optimization qo'shildi
 // CI/CD pipeline sozlandi
 // prettier formatlash
 // package.json yangilandi
