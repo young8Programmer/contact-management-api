c5b8208a0caf79811f6d5ef393a9b3ad09e9f5e5
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:16:30 +0500

    refactor: API response formatini yaxshilash

--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -1,3 +1,4 @@
+// environment variables sozlandi
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 // kod formatlash va indentatsiya
