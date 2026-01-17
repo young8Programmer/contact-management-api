2234e927efd80a3619ee273a64ef670fba93d9c2
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:16:59 +0500

    fix: memory leak muammosi hal qilindi

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 // kod formatlash va tozalash
+// package.json yangilandi
 // kod formatlash va indentatsiya
 async function bootstrap() {
 // database testlari qo'shildi
