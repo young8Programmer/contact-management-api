5664539f9f168c835d12e1f03c1c54f6308170bf
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:15:37 +0500

    docs: API hujjatlarini qo'shish

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -7,6 +7,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
+// bundle size optimallashtirildi
   let appController: AppController;
 // environment variables sozlandi
 
