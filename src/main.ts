435ca17c0207a43ef799322a264330f4a5f3041b
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:16:57 +0500

    style: kod uslubini yaxshilash

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+// database testlari qo'shildi
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
